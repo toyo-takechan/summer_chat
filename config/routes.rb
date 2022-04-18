4fe9c4122faf90decb8bc3f4c70c80f2f4d235b0
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2022-04-18 13:54:29 +0900

    session controller new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "home#top"
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'session#destroy'
   resources :users
 end
